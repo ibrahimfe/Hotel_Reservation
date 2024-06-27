2bd062cd89f54ffafca9d8c6663c216f2035677a
hasibashari <hasibashari@gmail.com>
2024-06-27 17:25:24 +0700

    Update code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from '../assets/logo.png';
+import React from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-[#EDEFFD] text-[#4464AD] p-4 shadow">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <img src={logo} alt="HotelGo Logo" className="h-8 mr-2" />
-          <span className="text-xl font-bold"></span>
+          <a href="./Hero.js">
+            <img src={logo} alt="HotelGo Logo" className="h-8 mr-2" />
+            <span className="text-xl font-bold"></span>
+          </a>
         </div>
         <div className="hidden md:flex">
-          <a href="#facilities" className="mx-2">Facilities</a>
-          <a href="#best-offer" className="mx-2">Best Offer</a>
-          <a href="#contact" className="mx-2">About</a>
-          <a href="#contact" className="mx-2">Contact</a>
+          <a href="#facilities" className="mx-2">
+            Facilities
+          </a>
+          <a href="#best-offer" className="mx-2">
+            Best Offer
+          </a>
+          <a href="#contact" className="mx-2">
+            About
+          </a>
+          <a href="#contact" className="mx-2">
+            Contact
+          </a>
+          <Link to="/payment">Payment</Link>
         </div>
         <button className="md:hidden">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
           </svg>
         </button>
       </div>
